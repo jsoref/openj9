1023ca1b406f806197aab4696e5e7b2ba3a0fe31
Josh Soref <jsoref@gmail.com>
2021-06-04 19:32:50 -0400

    spelling: chunk sizes
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- runtime/libffi/dlmalloc.c
+++ runtime/libffi/dlmalloc.c
@@ -1791,7 +1791,7 @@ nextchunk-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
             +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
   Larger chunks are kept in a form of bitwise digital trees (aka
-  tries) keyed on chunksizes.  Because malloc_tree_chunks are only for
+  tries) keyed on chunk sizes.  Because malloc_tree_chunks are only for
   free chunks greater than 256 bytes, their size doesn't impose any
   constraints on user chunk sizes.  Each node looks like:
 
