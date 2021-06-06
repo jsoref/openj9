2c5558c0aa25dd61270edb1cffe606ef36479b6d
Josh Soref <jsoref@gmail.com>
2021-06-06 00:32:48 -0400

    spelling: labelfall
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- runtime/compiler/p/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/p/codegen/J9TreeEvaluator.cpp
@@ -11868,7 +11868,7 @@ TR::Register *J9::Power::TreeEvaluator::tstartEvaluator(TR::Node *node, TR::Code
 
    TR::LabelSymbol *labelPersistentFailure = persistentFailureNode->getBranchDestination()->getNode()->getLabel();
    TR::LabelSymbol *labelTransientFailure  = transientFailureNode->getBranchDestination()->getNode()->getLabel();
-   TR::LabelSymbol *labelfallThrough  = fallThrough->getBranchDestination()->getNode()->getLabel();
+   TR::LabelSymbol *labelFallThrough  = fallThrough->getBranchDestination()->getNode()->getLabel();
    TR::LabelSymbol *startLabel = generateLabelSymbol(cg);
    TR::LabelSymbol *lockwordLabel = generateLabelSymbol(cg);
    TR::LabelSymbol *noTexStatsLabel = generateLabelSymbol(cg);
@@ -12010,9 +12010,9 @@ TR::Register *J9::Power::TreeEvaluator::tstartEvaluator(TR::Node *node, TR::Code
    else
       {
       if (fallThroughConditions)
-         generateDepLabelInstruction(cg, TR::InstOpCode::b, node, labelfallThrough, fallThroughConditions);
+         generateDepLabelInstruction(cg, TR::InstOpCode::b, node, labelFallThrough, fallThroughConditions);
       else
-         generateLabelInstruction(cg, TR::InstOpCode::b, node, labelfallThrough);
+         generateLabelInstruction(cg, TR::InstOpCode::b, node, labelFallThrough);
       }
 
    cg->stopUsingRegister(monReg);
