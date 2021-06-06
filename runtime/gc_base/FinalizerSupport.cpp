a93729c33cdc0970fb2be4890e05fba72a9064c2
Josh Soref <jsoref@gmail.com>
2021-06-06 00:27:52 -0400

    spelling: finalizer
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- runtime/gc_base/FinalizerSupport.cpp
+++ runtime/gc_base/FinalizerSupport.cpp
@@ -1051,7 +1051,7 @@ forceClassLoaderUnload(J9VMThread *vmThread, J9ClassLoader *classLoader)
 				tempNextThread->gcClassUnloadingThreadPrevious = vmThread;
 			}
 			
-			/* If we need to signal the finalzer to force unloading a class loader, do so */
+			/* If we need to signal the finalizer to force unloading a class loader, do so */
 			if ( setForceFlag ) {
 				omrthread_monitor_enter(jvm->finalizeMainMonitor);
 				jvm->finalizeMainFlags |= J9_FINALIZE_FLAGS_FORCE_CLASS_LOADER_UNLOAD;
